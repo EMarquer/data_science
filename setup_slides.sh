be49fb8d28534bde9ed049a1f06733e66e82ba81
Esteban Marquer <e.marquer1@gmail.com>
2019-05-01 19:52:53 +0200

    setup slides

---
+++ setup_slides.sh
@@ -0,0 +1,3 @@
+jupyter nbconvert --to slides index.ipynb --reveal-prefix=reveal.js --SlidesExporter.reveal_theme=serif 
+--SlidesExporter.reveal_scroll=True 
+--SlidesExporter.reveal_transition=none
